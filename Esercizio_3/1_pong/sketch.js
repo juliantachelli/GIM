e467a12d5f2b38b86d42c680cd7ef7a7e17321e1
juliantachelli <131649384+juliantachelli@users.noreply.github.com>
2023-05-14 15:35:22 +0200

    Caricati esercizi 3

--- Esercizio_3/1_pong/sketch.js
+++ Esercizio_3/1_pong/sketch.js
@@ -3,19 +3,24 @@ let posizioneX
 let velX
 let velY
 
+alert('ATTENZIONE:\n'+
+		  'Questa composizione, se guardata attentamente,\n'+
+		  'potrebbe farti perdere tanto tempo e crearti dipendenza.\n\n'+
+	    'BUONA VISIONE! :)');
+
 function setup(){
-	createCanvas(1470, 760)
+	createCanvas(1515, 801)
 	posizioneX = width/2
 	posizioneY = height/2 
-	velX = 3
-	velY = 2
-	background(0, 0, 0)
+	velX = 1.618
+	velY = -3.36
+	background(0, 0, 30)
 }
 
 function draw(){
 	noStroke()
 	//fill(map(posizioneX, 0, width, 0, 255), map(posizioneY, 0, width, 0, 255), 0)
-
+	// 0.031-32-33
 	const r = (sin(frameCount * 0.031) + 1) * 127.5
 	const g = (sin(frameCount * 0.032) + 1) * 127.5
 	const b = (sin(frameCount * 0.033) + 1) * 127.5
